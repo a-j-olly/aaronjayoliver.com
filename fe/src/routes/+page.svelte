28ff67b980014518c4631a40cbb416ef53f2ffc1
aoliver <aaronjayoliver@gmail.com>
2024-10-01 17:56:25 +0100

    Added tailwindcss

--- fe/src/routes/+page.svelte
+++ fe/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<nav class="flex justify-center">
+	<h1 class="text-4xl">Welcome to SvelteKit</h1>
+</nav>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
