dc3d0cf38289ce1624c4d7f79b0aa83903bdfa47
aoliver <aaronjayoliver@gmail.com>
2024-10-02 11:03:33 +0100

    Added landing page layout. Scaffolded out the portfolio page.

--- fe/src/routes/+page.svelte
+++ fe/src/routes/+page.svelte
@@ -1,5 +1,31 @@
-<nav class="flex justify-center">
-	<h1 class="text-4xl">Welcome to SvelteKit</h1>
-</nav>
+<div class="flex w-screen flex-col">
+	<div class="flex h-screen items-center justify-center">
+		<div class="flex h-min flex-row items-center justify-center divide-x-2">
+			<ul class="pr-3 text-right text-2xl font-bold">
+				<li>
+					<h1>Aaron</h1>
+				</li>
+				<li>
+					<h1>Jay</h1>
+				</li>
+				<li>
+					<h1>Oliver</h1>
+				</li>
+			</ul>
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+			<ul class="pl-3 text-2xl">
+				<li>
+					<h2><a href="#about">about</a></h2>
+				</li>
+				<li>
+					<h2><a href="/portfolio">portfolio</a></h2>
+				</li>
+				<li>
+					<h2><a href="/blog">blog</a></h2>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="flex h-screen" id="about">About Me</div>
+</div>
