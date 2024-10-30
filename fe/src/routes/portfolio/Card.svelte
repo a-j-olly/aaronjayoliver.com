e775df39863f63aee233d45fc64c5dd538526971
aoliver <aaronjayoliver@gmail.com>
2024-10-30 16:43:29 +0000

    Added images for existing project data

--- fe/src/routes/portfolio/Card.svelte
+++ fe/src/routes/portfolio/Card.svelte
@@ -2,13 +2,15 @@
 	export let name;
 	export let imageURL;
 	export let navURL;
+
+
 </script>
 
 <a
 	class="min-h-32 min-w-64 overflow-hidden rounded border-4 border-orange-400 bg-orange-400 shadow-lg hover:bg-orange-500 hover:border-orange-500"
 	href={navURL}
 >
-	<img src={imageURL} alt={name} class="w-full object-cover" />
+	<img src={`/images/projects/${imageURL}`} alt={name} class="w-full object-cover" />
 
 	<h2 class="text-center font-serif text-xl text-white">{name}</h2>
 </a>
