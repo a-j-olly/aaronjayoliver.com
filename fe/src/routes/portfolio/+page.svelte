6f821d67f50f4bb5260b0dacbff64be7bc057531
aoliver <aaronjayoliver@gmail.com>
2024-11-12 09:08:59 +0000

    Added show/hide functionality to the tag section of the portfolio.

--- fe/src/routes/portfolio/+page.svelte
+++ fe/src/routes/portfolio/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import type { TagItem } from 'shared_types';
 	import { projectStore, tagStore, selectedTagStore, displayedProjectStore } from '$lib/stores';
+	import { filterProjectListByTags } from '$lib';
+
 	import Card from './Card.svelte';
 	import Pill from './Pill.svelte';
-	import CloseIcon from './CloseIcon.svelte';
-	import { filterProjectListByTags } from '$lib';
+
+	import ClearIcon from './ClearIcon.svelte';
+	import MinimiseIcon from './MinimiseIcon.svelte';
+	import MaximiseIcon from './MaximiseIcon.svelte';
+
+	let showTags = $state(true);
+
+	function toggleShowTags() {
+		showTags = !showTags;
+	}
 
 	async function tagToggleHandler(event: CustomEvent<{ tag: TagItem; selected: boolean }>) {
 		const { tag, selected } = event.detail;
@@ -29,28 +39,44 @@
 
 <svelte:head>
 	<title>Portfolio | aaronjayoliver.com</title>
-	<meta name="description" content="This is the portfolio page for aaronjayoliver.com. It lists software development projects that I have developed." />
+	<meta
+		name="description"
+		content="This is the portfolio page for aaronjayoliver.com. It lists software development projects that I have developed."
+	/>
 </svelte:head>
 
 <div class="flex justify-center">
 	<div class="mx-2 max-w-screen-xl rounded bg-orange-200/50 pb-2 sm:mx-4 md:mx-8">
-		<div class="relative">
+		<div class="relative" class:hidden={!showTags}>
 			<h1 class="rounded-t bg-orange-400 text-center font-serif text-2xl text-white">Tags</h1>
-			<div class="absolute bottom-0 right-0 flex h-8 w-10 items-center justify-center">
+			<div class="absolute bottom-0 right-0">
 				<button
 					type="button"
 					disabled={$selectedTagStore.length === 0}
-					class="rounded text-center text-sm font-medium text-white {$selectedTagStore.length === 0
-						? 'bg-orange-400'
+					class="flex h-8 w-8 items-center justify-center rounded-tr text-white {$selectedTagStore.length ===
+					0
+						? 'bg-slate-400 text-white'
 						: 'bg-red-700 hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700'}"
 					onclick={clearTagsHandler}
-					><CloseIcon />
+				>
+					<ClearIcon />
+				</button>
+			</div>
+			<div class="absolute bottom-0 left-0">
+				<button
+					type="button"
+					disabled={!showTags}
+					class="flex h-8 w-8 items-center justify-center bg-orange-400 bg-red-700 text-white hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700"
+					onclick={toggleShowTags}
+				>
+					<MinimiseIcon height="24px" width="24px" />
 				</button>
 			</div>
 		</div>
 
 		<ul
 			class="grid grid-cols-[repeat(auto-fit,_minmax(96px,_max-content))] justify-center gap-1 p-1 xl:grid-cols-[repeat(auto-fit,_minmax(112px,_max-content))]"
+			class:hidden={!showTags}
 		>
 			{#each $tagStore as tag}
 				<li>
@@ -59,7 +85,24 @@
 			{/each}
 		</ul>
 
-		<h1 class="mb-2 bg-orange-400 text-center font-serif text-2xl text-white">Projects</h1>
+		<div class="relative">
+			<h1
+				class="mb-2 bg-orange-400 text-center font-serif text-2xl text-white"
+				class:rounded-t={!showTags}
+			>
+				Projects
+			</h1>
+			<div class="absolute bottom-0 left-0" class:hidden={showTags}>
+				<button
+					type="button"
+					disabled={showTags}
+					class="flex h-8 w-8 items-center justify-center rounded-tr bg-orange-400 bg-red-700 text-white hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700"
+					onclick={toggleShowTags}
+				>
+					<MaximiseIcon height="24px" width="24px" />
+				</button>
+			</div>
+		</div>
 
 		{#if $displayedProjectStore.length}
 			<ul class="m-auto grid grid-cols-1 gap-4 px-2 sm:grid-cols-2">
