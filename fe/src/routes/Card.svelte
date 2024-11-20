8beacf117b0b240560f5b62a8fde30f4704e62f5
aoliver <aaronjayoliver@gmail.com>
2024-11-20 12:19:48 +0000

    Improved lighthouse accessibility score from 83 to 94

--- fe/src/routes/Card.svelte
+++ fe/src/routes/Card.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Picture } from "shared_types";
+	import type { Picture } from 'shared_types';
 
 	interface Props {
 		name: string;
@@ -10,10 +10,7 @@
 	const navURL = `/${encodeURI(name)}`;
 </script>
 
-<a
-	class="min-h-32 min-w-64 overflow-hidden rounded border-4 border-orange-400 bg-orange-400 shadow-lg hover:border-orange-500 hover:bg-orange-500"
-	href={navURL}
->
+<a href={navURL}>
 	<enhanced:img src={image} alt={name} class="w-full object-cover" />
 
 	<h2 class="text-center font-serif text-xl text-white">{name}</h2>
