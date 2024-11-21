75f1f903a5f5ec9af6d9a5697515a41c724dc435
aoliver <aaronjayoliver@gmail.com>
2024-11-21 13:47:46 +0000

    Improved project slugs

--- fe/src/routes/Card.svelte
+++ fe/src/routes/Card.svelte
@@ -3,15 +3,16 @@
 
 	interface Props {
 		name: string;
+		slug: string
 		image: string | Picture;
 	}
 
-	let { name, image }: Props = $props();
-	const navURL = `/${encodeURI(name)}`;
+	let { name, image, slug }: Props = $props();
+	const navURL = `/${encodeURI(slug)}`;
 </script>
 
 <a href={navURL}>
-	<enhanced:img src={image} alt={name} class="w-full object-cover" />
+	<enhanced:img src={image} alt="" class="w-full object-cover" />
 
 	<h2 class="text-center font-serif text-xl text-white">{name}</h2>
 </a>
